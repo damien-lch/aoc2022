148a287efb80b05acdddab9fb30ed4a0f9a04caa
neiwad <hello@neiwad.dev>
2022-12-07 15:57:32 -0500

    day7, refactor needed

--- day7/part1.js
+++ day7/part1.js
@@ -17,36 +17,43 @@ const getCommand = (string) => {
     .filter((s) => s);
 };
 
+class Directory {
+  name = "";
+  parent = null;
+  children = [];
+  files = [];
+  constructor(name, parent) {
+    this.name = name;
+    this.parent = parent || null;
+  }
+  addChild = (child) => {
+    this.children = [...this.children, child];
+  };
+  addFile = (file) => {
+    this.files = [...this.files, file];
+  };
+  getFilesSize = () => {
+    return this.files.reduce((acc, cur) => {
+      return acc + parseInt(cur.size);
+    }, 0);
+  };
+}
+
 const getNewDirectory = (name) => {
-  if (name === "..") {
-    currentDir = directories.find((d) => d.name === currentDir.parent);
+  if (name === "/") {
+    const newDir = new Directory("/");
+    directories.push(newDir);
+    currentDir = newDir;
   } else {
-    if (name === "/") {
-      directories.push({
-        name: "/",
-        parent: null,
-        files: [],
-        size: 0,
-      });
-      currentDir = directories.find((d) => d.name === name);
+    if (name === "..") {
+      currentDir = currentDir.parent;
     } else {
-      if (!directories.find((d) => d.name === name)) {
-        directories.push({
-          name: name,
-          parent: currentDir ? currentDir.name : null,
-          files: [],
-          size: 0,
-        });
-      }
-      currentDir = directories.find(
-        (d) => d.name === name && d.parent === currentDir.name
-      );
+      currentDir = currentDir.children.find((c) => c.name === name);
     }
   }
 };
 
 arr.forEach((line, i) => {
-  console.log(i);
   if (isCommand(line)) {
     const [command, option] = getCommand(line);
     switch (command) {
@@ -62,53 +69,36 @@ arr.forEach((line, i) => {
     if (currentCmd === "ls") {
       const [line1, line2] = line.split(" ");
       if (line1 === "dir") {
-        //Check if folder already exist
-        if (
-          !directories.find(
-            (d) => d.name === line2 && d.parent === currentDir.name
-          )
-        ) {
-          console.log("current dir", currentDir);
-          directories.push({
-            name: line2,
-            parent: currentDir.name,
-            files: [],
-            size: 0,
-          });
-        }
+        const newDir = new Directory(line2, currentDir);
+        currentDir.addChild(newDir);
+        directories.push(newDir);
       } else {
-        currentDir.files = [
-          ...currentDir.files,
-          {
-            name: line2,
-            size: line1,
-          },
-        ];
-        currentDir.size += parseInt(line1);
+        currentDir.addFile({
+          name: line2,
+          size: line1,
+        });
       }
     }
   }
-  console.log(directories);
 });
 
 let tempTotal = 0;
 const findChilds = (dir) => {
-  tempTotal += parseInt(dir.size);
-  const childs = directories.filter((d) => d.parent === dir.name);
-  childs.forEach((c) => {
+  tempTotal += parseInt(dir.getFilesSize());
+  dir.children.forEach((c) => {
     findChilds(c);
   });
   return tempTotal;
 };
 
-directories = directories.map((dir) => {
+const dirs = directories.map((dir) => {
   const iciTotal = findChilds(dir);
   tempTotal = 0;
   return { ...dir, grandTotal: iciTotal };
 });
 
 console.log(
-  directories.reduce((acc, curr) => {
+  dirs.reduce((acc, curr) => {
     return curr.grandTotal < 100_000 ? (acc += parseInt(curr.grandTotal)) : acc;
   }, 0)
 );
--- day7/part2.js
+++ day7/part2.js
@@ -1,3 +1,109 @@
 const { readFileSync } = require("fs");
 const contents = readFileSync("input.txt", "utf-8");
 const arr = contents.split(/\r?\n/);
+
+let directories = [];
+let currentDir;
+let currentCmd;
+let filesystem_size = 70_000_000;
+let update_space_needed = 30_000_000;
+
+const isCommand = (string) => {
+  return string[0] === "$";
+};
+
+const getCommand = (string) => {
+  return string
+    .split(" ")
+    .map((s) => s.replace("$", ""))
+    .filter((s) => s);
+};
+
+class Directory {
+  name = "";
+  parent = null;
+  children = [];
+  files = [];
+  constructor(name, parent) {
+    this.name = name;
+    this.parent = parent || null;
+  }
+  addChild = (child) => {
+    this.children = [...this.children, child];
+  };
+  addFile = (file) => {
+    this.files = [...this.files, file];
+  };
+  getFilesSize = () => {
+    return this.files.reduce((acc, cur) => {
+      return acc + parseInt(cur.size);
+    }, 0);
+  };
+}
+
+const getNewDirectory = (name) => {
+  if (name === "/") {
+    const newDir = new Directory("/");
+    directories.push(newDir);
+    currentDir = newDir;
+  } else {
+    if (name === "..") {
+      currentDir = currentDir.parent;
+    } else {
+      currentDir = currentDir.children.find((c) => c.name === name);
+    }
+  }
+};
+
+arr.forEach((line, i) => {
+  if (isCommand(line)) {
+    const [command, option] = getCommand(line);
+    switch (command) {
+      case "cd":
+        getNewDirectory(option);
+        currentCmd = command;
+        break;
+      case "ls":
+        currentCmd = command;
+        break;
+    }
+  } else {
+    if (currentCmd === "ls") {
+      const [line1, line2] = line.split(" ");
+      if (line1 === "dir") {
+        const newDir = new Directory(line2, currentDir);
+        currentDir.addChild(newDir);
+        directories.push(newDir);
+      } else {
+        currentDir.addFile({
+          name: line2,
+          size: line1,
+        });
+      }
+    }
+  }
+});
+
+let tempTotal = 0;
+const findChilds = (dir) => {
+  tempTotal += parseInt(dir.getFilesSize());
+  dir.children.forEach((c) => {
+    findChilds(c);
+  });
+  return tempTotal;
+};
+
+const dirs = directories.map((dir) => {
+  const iciTotal = findChilds(dir);
+  tempTotal = 0;
+  return { ...dir, grandTotal: iciTotal };
+});
+
+const freeSpace = filesystem_size - dirs.find((d) => d.name === "/").grandTotal;
+
+if (freeSpace < update_space_needed) {
+  const spaceNeeded = update_space_needed - freeSpace;
+  const deletableDirs = dirs.filter((d) => d.grandTotal >= spaceNeeded);
+  const minDirsSize = Math.min(...deletableDirs.map((d) => d.grandTotal));
+  console.log(minDirsSize);
+}
